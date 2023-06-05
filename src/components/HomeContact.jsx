f1b3fe747fa5ab4effc4bfe3518bcc1f9d6eb5b1
pierregoaer <hello@pierregoaer.com>
2023-06-04 20:23:37 -0400

    Removing contact form until CORS solved

--- src/components/HomeContact.jsx
+++ src/components/HomeContact.jsx
@@ -7,45 +7,46 @@ export default function HomeContact() {
             <div className={styles.homeContactInner}>
                 <div className={styles.introText}>
                     <h2>Contact</h2>
-                    <p>Feel free to get in touch on <a href="https://www.linkedin.com/in/pierregoaer/"
+                    <p>I can be reached on social media (<a href="https://www.linkedin.com/in/pierregoaer/"
                                                        target="_blank"
                                                        className="text-link">LinkedIn
                     </a>, <a
                         href="https://twitter.com/pierregoaer"
                         target="_blank"
-                        className="text-link">Twitter
+                        className="text-link">Twitter)
                     </a> or via email at <a href="mailto:hello@pierregoaer.com"
                                             target="_blank"
                                             className="text-link">hello@pierregoaer.com</a>.
                     </p>
-                    <p>You can also fill out this form and I will get back to you shortly.</p>
+                    <a href="mailto:hello@pierregoaer.com" className={`btn-primary ${styles.cta}`}>Get in Touch</a>
+                    {/*<p>You can also fill out this form and I will get back to you shortly.</p>*/}
                 </div>
-                <form name="contact"  method="POST" data-netlify="true" >
-                    <div className={styles.contactFormField}>
-                        <label htmlFor="name">Name</label>
-                        <input
-                            type="text"
-                            placeholder="Your full name"
-                            name="name"
-                        />
-                    </div>
-                    <div className={styles.contactFormField}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="text"
-                            placeholder="Your email"
-                            name="email"
-                        />
-                    </div>
-                    <div className={styles.contactFormField}>
-                        <label htmlFor="message">Message</label>
-                        <textarea
-                            placeholder="How can I help you?"
-                            name="message"
-                        />
-                    </div>
-                    <button type="submit" className="btn-tertiary">Send</button>
-                </form>
+                {/*<form name="contact"  method="POST" data-netlify="true" >*/}
+                {/*    <div className={styles.contactFormField}>*/}
+                {/*        <label htmlFor="name">Name</label>*/}
+                {/*        <input*/}
+                {/*            type="text"*/}
+                {/*            placeholder="Your full name"*/}
+                {/*            name="name"*/}
+                {/*        />*/}
+                {/*    </div>*/}
+                {/*    <div className={styles.contactFormField}>*/}
+                {/*        <label htmlFor="email">Email</label>*/}
+                {/*        <input*/}
+                {/*            type="text"*/}
+                {/*            placeholder="Your email"*/}
+                {/*            name="email"*/}
+                {/*        />*/}
+                {/*    </div>*/}
+                {/*    <div className={styles.contactFormField}>*/}
+                {/*        <label htmlFor="message">Message</label>*/}
+                {/*        <textarea*/}
+                {/*            placeholder="How can I help you?"*/}
+                {/*            name="message"*/}
+                {/*        />*/}
+                {/*    </div>*/}
+                {/*    <button type="submit" className="btn-tertiary">Send</button>*/}
+                {/*</form>*/}
             </div>
         </section>
     )
